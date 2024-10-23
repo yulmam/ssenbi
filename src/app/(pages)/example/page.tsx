94a037677fbe826c8914512d6e740e694fc704ec
yulmam <yulmam1@naver.com>
2024-10-23 13:58:03 +0900

    fix : 파일 함수 명 수정

--- src/app/(pages)/example/page.tsx
+++ src/app/(pages)/example/page.tsx
@@ -1,7 +1,7 @@
 import ContentCard from "@/app/components/common/ContentCard";
 import ResponsiveTabBar from "@/app/components/layout/ResponsiveTabBar";
 
-export default function Default() {
+export default function Example() {
   return (
     <>
       <ContentCard />
