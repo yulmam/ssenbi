3d7ef51c96d142373a7cdc4b5df59c8d6324fb8e
yulmam <yulmam1@naver.com>
2024-10-23 14:52:47 +0900

    style: 스타일 조정

--- src/app/components/layout/ResponsiveTabBar.tsx
+++ src/app/components/layout/ResponsiveTabBar.tsx
@@ -13,32 +13,33 @@ export default function ResponsiveTabBar() {
         />
         <p className="menu-bar_menu-text">템플릿</p>
       </div>
+
       <div className="menu-bar__menu">
         <Image
-          src="/assets/svg/File.svg"
+          src="/assets/svg/Star.svg"
           width={20}
           height={20}
-          alt="템플릿_icon"
+          alt="스크랩_icon"
         />
-        <p className="menu-bar_menu-text">템플릿</p>
+        <p className="menu-bar_menu-text">스크랩</p>
       </div>
       <div className="menu-bar__menu">
         <Image
-          src="/assets/svg/File.svg"
+          src="/assets/svg/Users.svg"
           width={20}
           height={20}
-          alt="템플릿_icon"
+          alt="고객_icon"
         />
-        <p className="menu-bar_menu-text">템플릿</p>
+        <p className="menu-bar_menu-text">고객</p>
       </div>
       <div className="menu-bar__menu">
         <Image
-          src="/assets/svg/File.svg"
+          src="/assets/svg/Smile.svg"
           width={20}
           height={20}
-          alt="템플릿_icon"
+          alt="Smile_icon"
         />
-        <p className="menu-bar_menu-text">템플릿</p>
+        <p className="menu-bar_menu-text">{"마이페이지"}</p>
       </div>
     </div>
   );
