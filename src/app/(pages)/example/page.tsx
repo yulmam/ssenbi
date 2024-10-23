27fb63ebfb66126005cedacbbda4bbb7c6a68834
yulmam <yulmam1@naver.com>
2024-10-23 15:02:14 +0900

    style : 페이지 width 조정

--- src/app/(pages)/example/page.tsx
+++ src/app/(pages)/example/page.tsx
@@ -1,10 +1,14 @@
 import ContentCard from "@/app/components/common/ContentCard";
-import ResponsiveTabBar from "@/app/components/layout/ResponsiveTabBar";
+
+/*
+  모든 페이지는 
+  width: "calc(100vw - 100px)" or var(--divWidth), marginLeft: 100  있어야함!!
+*/
 
 export default function Example() {
   return (
-    <>
+    <div style={{ width: "var(--divWidth)", marginLeft: 100 }}>
       <ContentCard />
-    </>
+    </div>
   );
 }
