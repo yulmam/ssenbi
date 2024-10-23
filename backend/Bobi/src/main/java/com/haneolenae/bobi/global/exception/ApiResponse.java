d2c55c94ee5bb162bc8b473f196a04f8c7125512
yulmam <yulmam1@naver.com>
2024-10-23 22:52:45 +0900

    feat: 오류 코드 템플릿 구현

---
+++ backend/Bobi/src/main/java/com/haneolenae/bobi/global/exception/ApiResponse.java
@@ -0,0 +1,41 @@
+package com.haneolenae.bobi.global.exception;
+
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+import lombok.Getter;
+
+@Getter
+@JsonPropertyOrder({"status", "code", "message", "result"})
+public class ApiResponse<T> {
+
+	private final int status;
+	private final String code;
+	private final String message;
+	private final T result;
+
+	public ApiResponse(ApiType apiType, T result) {
+		this.status = apiType.getStatus().value();
+		this.code = apiType.getCode().toString();
+		this.message = apiType.getMessage();
+		this.result = result;
+	}
+
+	public ApiResponse(T result) {
+		this.status = ApiType.SUCCESS.getStatus().value();
+		this.code = ApiType.SUCCESS.getCode().toString();
+		this.message = ApiType.SUCCESS.getMessage();
+		this.result = result;
+	}
+
+	public static ApiResponse<String> ok() {
+		return new ApiResponse<>(ApiType.SUCCESS, "");
+	}
+
+	public static ApiResponse<String> created() {
+		return new ApiResponse<>(ApiType.CREATED, "");
+	}
+
+	public static ApiResponse<String> serverError() {
+		return new ApiResponse<>(ApiType.SERVER_ERROR, "");
+	}
+}
---
+++ backend/Bobi/src/main/java/com/haneolenae/bobi/global/filter/ApiExceptionHandler.java
@@ -0,0 +1,38 @@
+package com.haneolenae.bobi.global.filter;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import com.haneolenae.bobi.global.exception.ApiException;
+import com.haneolenae.bobi.global.exception.ApiResponse;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestControllerAdvice
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
+
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<ApiResponse<String>> handleException(Exception ex) {
+		ex.printStackTrace();
+
+		log.error("Exception: {}", ex);
+		return ResponseEntity
+			.status(HttpStatus.INTERNAL_SERVER_ERROR)
+			.body(ApiResponse.serverError());
+	}
+
+	@ExceptionHandler(ApiException.class)
+	public ResponseEntity<ApiResponse<Object>> handleCommonApiException(ApiException ex) {
+		ex.printStackTrace();
+		log.error("ApiException: {}", ex);
+
+		return ResponseEntity
+			.status(ex.getApiType().getStatus())
+			.body(new ApiResponse<Object>(ex.getApiType(), ex.getResult()));
+	}
+
+}
