8d1922f5dc1bd235744cd8abc04a46d6102685fd
yulmam <yulmam1@naver.com>
2024-10-23 16:05:43 +0900

    feat: input field 구현 #S11P31A109-4 #done

--- src/app/(pages)/example/page.tsx
+++ src/app/(pages)/example/page.tsx
@@ -1,4 +1,5 @@
 import ContentCard from "@/app/components/common/ContentCard";
+import InputField from "@/app/components/common/InputField";
 // import ResponsiveTabBar from "@/app/components/layout/ResponsiveTabBar";
 
 export default function Default() {
@@ -9,6 +10,8 @@ export default function Default() {
         title={"제목"}
         description={"설명"}
       />
+      <InputField label="이메일" type="email" />
+      <InputField label="비밀번호" type="password" />
       {/* <ResponsiveTabBar /> */}
     </>
   );
