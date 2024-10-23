80940ee315a96f40f6d40cf15739d6852c5ba1cc
yulmam <yulmam1@naver.com>
2024-10-23 13:44:28 +0900

    fix : 폴더 구조

---
+++ src/app/(pages)/example/page.tsx
@@ -0,0 +1,11 @@
+import ContentCard from "@/app/components/common/ContentCard";
+import ResponsiveTabBar from "@/app/components/layout/ResponsiveTabBar";
+
+export default function Default() {
+  return (
+    <>
+      <ContentCard />
+      <ResponsiveTabBar />
+    </>
+  );
+}
