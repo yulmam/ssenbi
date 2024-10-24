2f7418ff829a3c770dce1fa826f4fc61a874ea8d
yulmam <yulmam1@naver.com>
2024-10-24 10:03:27 +0900

    style: 불필요한 주석 제거

--- src/app/(pages)/template/page.tsx
+++ src/app/(pages)/template/page.tsx
@@ -3,13 +3,6 @@
 import NavigationBar from "@/app/components/common/NavigationBar";
 import { useState } from "react";
 
-/*
-필수!!
-  모든 페이지는 첫 줄에
- <div className="page-container"> 필수
- bottom 바 스타일 조정때문!! 
-*/
-
 export default function Template() {
   const [currentTab, setCurrentTab] = useState<number>(0);
 
