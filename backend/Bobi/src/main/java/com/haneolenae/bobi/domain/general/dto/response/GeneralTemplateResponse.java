2076ec8eb5b5312164d2027f5beaa2b288169024
yulmam <yulmam1@naver.com>
2024-10-25 11:18:37 +0900

    feat: dto 개발

---
+++ backend/Bobi/src/main/java/com/haneolenae/bobi/domain/general/dto/response/GeneralTemplateResponse.java
@@ -0,0 +1,17 @@
+package com.haneolenae.bobi.domain.general.dto.response;
+
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class GeneralTemplateResponse {
+    private long templateId;
+    private String templateTitle;
+    private String templateContent;
+    private long count;
+    private LocalDateTime createdAt;
+}
