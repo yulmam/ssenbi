2ecf530be5d07f21e36feb6c026098cbef9103b4
yulmam <yulmam1@naver.com>
2024-10-25 09:56:28 +0900

    feat: tag 구현 #S11P31A109-16 #done

--- src/app/(pages)/example/page.tsx
+++ src/app/(pages)/example/page.tsx
@@ -1,5 +1,6 @@
 import ContentCard from "@/app/components/common/ContentCard";
 import InputField from "@/app/components/common/InputField";
+import Tag from "@/app/components/common/Tag";
 
 /*
 필수!!
@@ -17,6 +18,18 @@ export default function Example() {
         description={"설명"}
       />
       <InputField label="이메일" type="email" />
+      <Tag color="RED" tagName="빨강" />
+      <Tag color="PINK" tagName="핑크" />
+      <Tag color="SALMON" tagName="살몬" />
+      <Tag color="ORANGE" tagName="주황" />
+      <Tag color="YELLOW" tagName="노랑" />
+      <Tag color="LIME" tagName="라임" />
+      <Tag color="GREEN" tagName="초록" />
+      <Tag color="SKYBLUE" tagName="하늘" />
+      <Tag color="BLUE" tagName="파랑" />
+      <Tag color="PURPLE" tagName="보라" />
+      <Tag color="BEIGE" tagName="베이지" />
+      <Tag color="GRAY" tagName="회색" />
     </div>
   );
 }
--- src/app/components/common/Banner.tsx
+++ src/app/components/common/Banner.tsx
@@ -2,9 +2,5 @@ import React from "react";
 import "./Banner.css";
 
 export default function NavigationBar() {
-  return (
-    <div className="banner">
-      이러쿵 저러쿵
-    </div>
-  );
+  return <div className="banner">이러쿵 저러쿵</div>;
 }
