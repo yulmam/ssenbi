2f8ed0885d3d6ac1836db7173313f316884d72a6
yulmam <yulmam1@naver.com>
2024-10-28 10:24:26 +0900

    fix : file position

--- src/app/(pages)/example/page.tsx
+++ src/app/(pages)/example/page.tsx
@@ -1,5 +1,5 @@
-import ContentCard from "@/app/components/common/ContentCard";
-import InputField from "@/app/components/common/InputField";
+import ContentCard from "@/app/components/common/card/ContentCard";
+import InputField from "@/app/components/common/input/InputField";
 import BorderTag from "@/app/components/common/tag/BorderTag";
 import FilledTag from "@/app/components/common/tag/FilledTag";
 import CustomerCard from "@/app/components/common/card/CustomerCard";
