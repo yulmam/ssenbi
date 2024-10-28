47f9bfee90a3de6d7d2cf060dd0de9f877d7d782
yulmam <yulmam1@naver.com>
2024-10-28 10:59:41 +0900

    style : serchBar 위치 스타일

--- src/app/(pages)/message/page.tsx
+++ src/app/(pages)/message/page.tsx
@@ -4,8 +4,8 @@ export default function MessagePage() {
   const onChan = () => {};
 
   return (
-    <>
+    <div className="page-container">
       <SearchBar onChange={onChan} placeholder="검색어 (이름, 제목, 태그)" />
-    </>
+    </div>
   );
 }
