f1693e606a0ced46c67d7ce0ddd18734d98b769a
yulmam <yulmam1@naver.com>
2024-10-28 11:53:29 +0900

    feat : searchBar + debounce

--- src/app/components/common/input/SearchBar.tsx
+++ src/app/components/common/input/SearchBar.tsx
@@ -1,11 +1,15 @@
+"use client";
+
+import React, { useMemo, useEffect } from "react";
+import debounce from "lodash/debounce";
 import "./SearchBar.css";
 import ReadingGlassesIcon from "@/app/assets/svg/ReadingGlasses.svg";
 
 interface SearchBarProps {
   type?: "text" | "number";
-  value?: "" | 0;
+  value?: string | number;
   placeholder?: string;
-  onChange: () => void;
+  onChange: (value: string | number) => void;
 }
 
 export default function SearchBar({
@@ -14,13 +18,25 @@ export default function SearchBar({
   placeholder,
   onChange,
 }: SearchBarProps) {
+  const debouncedOnChange = useMemo(() => debounce(onChange, 300), [onChange]);
+
+  useEffect(() => {
+    return () => {
+      debouncedOnChange.cancel();
+    };
+  }, [debouncedOnChange]);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    debouncedOnChange(e.target.value);
+  };
+
   return (
     <div className="search-bar">
       <input
         type={type}
         placeholder={placeholder}
         value={value}
-        // onChange={onChange}
+        onChange={handleChange}
         className="search-input"
       />
       <ReadingGlassesIcon className="search-icon" viewBox="0 0 24 24" />
