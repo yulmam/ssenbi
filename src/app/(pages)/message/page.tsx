f1248b57c72b5278227a16e3d2048387b7df13c7
yulmam <yulmam1@naver.com>
2024-10-28 10:49:59 +0900

    feat : message 페이지 구축

--- src/app/(pages)/message/page.tsx
+++ src/app/(pages)/message/page.tsx
@@ -1,3 +1,11 @@
+import SearchBar from "@/app/components/common/input/SearchBar";
+
 export default function MessagePage() {
-  return <></>;
+  const onChan = () => {};
+
+  return (
+    <>
+      <SearchBar onChange={onChan} placeholder="검색어 (이름, 제목, 태그)" />
+    </>
+  );
 }
