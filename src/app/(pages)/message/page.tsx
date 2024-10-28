761e0565b83a2ccb483ef6f3876ec9a031bab441
yulmam <yulmam1@naver.com>
2024-10-28 11:54:21 +0900

    feat : messagePage _searchBar

--- src/app/(pages)/message/page.tsx
+++ src/app/(pages)/message/page.tsx
@@ -1,11 +1,20 @@
+"use client";
+
 import SearchBar from "@/app/components/common/input/SearchBar";
 
 export default function MessagePage() {
-  const onChan = () => {};
+  const handleChange = (value: string | number) => {
+    // todo : api에서 정보 받기
+  };
 
   return (
     <div className="page-container">
-      <SearchBar onChange={onChan} placeholder="검색어 (이름, 제목, 태그)" />
+      <SearchBar
+        type="text"
+        value={""}
+        onChange={handleChange}
+        placeholder="검색어 (이름, 제목, 태그)"
+      />
     </div>
   );
 }
