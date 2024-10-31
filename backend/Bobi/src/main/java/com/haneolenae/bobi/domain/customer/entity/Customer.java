95e45799f89dbc1762788ded02666dd0aec1daf8
yulmam <yulmam1@naver.com>
2024-10-31 16:44:18 +0900

    feat: customer와 member 연결

--- backend/Bobi/src/main/java/com/haneolenae/bobi/domain/customer/entity/Customer.java
+++ backend/Bobi/src/main/java/com/haneolenae/bobi/domain/customer/entity/Customer.java
@@ -7,6 +7,7 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import com.haneolenae.bobi.domain.custom.entity.TemplateCustomer;
+import com.haneolenae.bobi.domain.member.entity.Member;
 import com.haneolenae.bobi.domain.tag.entity.Tag;
 
 import jakarta.persistence.Column;
@@ -15,6 +16,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
@@ -65,8 +67,8 @@ public class Customer {
 	@OneToMany(mappedBy = "customer")
 	private List<TemplateCustomer> templateCustomers;
 
-	//@ManyToOne
-	//private Member member;
+	@ManyToOne
+	private Member member;
 
 	@Override
 	public boolean equals(Object o) {
