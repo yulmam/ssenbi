f76f08329b0121da6e512c4caceb28d82c25237f
yulmam <yulmam1@naver.com>
2024-10-31 16:20:22 +0900

    refactor: tag list 리팩토링

--- src/app/components/common/tag/TagList.tsx
+++ src/app/components/common/tag/TagList.tsx
@@ -7,6 +7,7 @@ import getRandomTagColor from "@/utils/getRandomTagColor";
 import * as Popover from "@radix-ui/react-popover";
 import { TagType } from "@/types/tag/tagTypes";
 import FilledTag from "./FilledTag";
+import TabBar from "./TabBar";
 
 interface TagListProps {
   tags?: TagType[];
@@ -14,128 +15,133 @@ interface TagListProps {
   maxTagCount?: number;
 }
 
+const TAG_TABS = ["태그", "고객"];
+const TAG_PREFIX = {
+  태그: "#",
+  고객: "@",
+};
+
 export default function TagList({
   tags = [],
   customers = [],
   maxTagCount = Infinity,
 }: TagListProps) {
-  const [activeTags, setActiveTags] = useState<TagType[]>(tags);
-  const [activeCustomers, setActiveCustomers] = useState<TagType[]>(customers);
-  const inputRef = useRef<HTMLInputElement>(null);
-  const [tab, setTab] = useState<"tag" | "customer">("tag");
-
+  const [validItems, setValidItems] = useState<{ [key: string]: TagType[] }>({
+    [TAG_TABS[0]]: tags,
+    [TAG_TABS[1]]: customers,
+  });
+  const [tab, setTab] = useState<string>(TAG_TABS[0]);
   const [editingItem, setEditingItem] = useState<{
-    type: "tag" | "customer";
+    type: string;
     index: number;
     text: string;
   } | null>(null);
-
-  const triggerRef = useRef<HTMLUListElement>(null);
   const [triggerWidth, setTriggerWidth] = useState<number>(0);
+  const inputRef = useRef<HTMLInputElement>(null);
+  const triggerRef = useRef<HTMLUListElement>(null);
 
   useEffect(() => {
     if (triggerRef.current) {
       setTriggerWidth(triggerRef.current.offsetWidth);
     }
-  }, [activeTags, activeCustomers]);
+  }, []);
 
-  function handleKeyDown(event: React.KeyboardEvent<HTMLInputElement>) {
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
     if (event.key === "Enter" && isValidInput(event.currentTarget.value)) {
       addItem(event.currentTarget.value);
       event.currentTarget.value = "";
     }
-  }
-
-  function isValidInput(name: string): boolean {
+  };
+  const isValidInput = (name: string): boolean => {
     const trimmedName = name.trim();
     if (!trimmedName) return false;
 
-    const list = tab === "tag" ? activeTags : activeCustomers;
+    const list = validItems[tab];
 
     return !list.some(
       (item) => item.tagName.toLowerCase() === trimmedName.toLowerCase(),
     );
-  }
-
-  function addItem(name: string) {
+  };
+  const addItem = (name: string) => {
     const newItemName = name.trim();
     const newItem: TagType = {
       tagName: newItemName,
       tagColor: getRandomTagColor(),
     };
 
-    if (tab === "tag") {
-      setActiveTags((prev) => [...prev, newItem]);
-    } else {
-      setActiveCustomers((prev) => [...prev, newItem]);
-    }
-  }
-
-  function saveEditedItem() {
+    setValidItems((prev) => ({
+      ...prev,
+      [tab]: [...prev[tab], newItem],
+    }));
+  };
+  const saveEditedItem = () => {
     if (!editingItem) return;
+
     const trimmedName = editingItem.text.trim();
+
     if (!trimmedName) {
       setEditingItem(null);
       return;
     }
 
-    if (editingItem.type === "tag") {
-      if (
-        activeTags.some(
-          (item, idx) =>
-            item.tagName.toLowerCase() === trimmedName.toLowerCase() &&
-            idx !== editingItem.index,
-        )
-      ) {
-        setEditingItem(null);
-        return;
-      }
-      setActiveTags((prev) =>
-        prev.map((item, idx) =>
-          idx === editingItem.index ? { ...item, tagName: trimmedName } : item,
-        ),
-      );
-    } else {
-      if (
-        activeCustomers.some(
-          (item, idx) =>
-            item.tagName.toLowerCase() === trimmedName.toLowerCase() &&
-            idx !== editingItem.index,
-        )
-      ) {
-        setEditingItem(null);
-        return;
-      }
-      setActiveCustomers((prev) =>
-        prev.map((item, idx) =>
-          idx === editingItem.index ? { ...item, tagName: trimmedName } : item,
-        ),
-      );
+    if (
+      validItems[editingItem.type].some(
+        (item, idx) =>
+          item.tagName.toLowerCase() === trimmedName.toLowerCase() &&
+          idx !== editingItem.index,
+      )
+    ) {
+      setEditingItem(null);
+      return;
     }
+    setValidItems((prev) => ({
+      ...prev,
+      [editingItem.type]: prev[editingItem.type].map((item, idx) =>
+        idx === editingItem.index ? { ...item, tagName: trimmedName } : item,
+      ),
+    }));
+
     setEditingItem(null);
-  }
+  };
+  const saveEditedItemOnEnter = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      saveEditedItem();
+    }
+  };
+  const handleSetEditingItem = (text: string, index: number) => () =>
+    setEditingItem({
+      type: tab,
+      index,
+      text,
+    });
+  const handleDeleteTag = (index: number) => () => {
+    setValidItems((prev) => ({
+      ...prev,
+      [tab]: prev[tab].filter((_, i) => i !== index),
+    }));
+  };
 
   return (
     <div className="tag-list-wrapper">
       <Popover.Root>
         <Popover.Trigger asChild>
           <ul ref={triggerRef} className="tag-list pointer">
-            {activeTags.slice(0, maxTagCount).map((tag) => (
-              <li key={`tag-${tag.tagName}-${tag.tagColor}`}>
-                <BorderTag color={tag.tagColor} tagName={tag.tagName} />
-              </li>
-            ))}
-            {activeCustomers.slice(0, maxTagCount).map((customer) => (
-              <li key={`customer-${customer.tagName}-${customer.tagColor}`}>
-                <FilledTag
-                  color={customer.tagColor}
-                  tagName={customer.tagName}
-                />
-              </li>
-            ))}
-            {activeTags.length + activeCustomers.length > maxTagCount && (
+            {Object.keys(validItems).map((tab) =>
+              validItems[tab]
+                .slice(0, maxTagCount)
+                .map((tag) => (
+                  <li key={`${tab}-${tag.tagName}-${tag.tagColor}`}>
+                    {tab === "태그" ? (
+                      <BorderTag color={tag.tagColor} tagName={tag.tagName} />
+                    ) : (
+                      <FilledTag color={tag.tagColor} tagName={tag.tagName} />
+                    )}
+                  </li>
+                )),
+            )}
+            {validItems[tab].length > maxTagCount && (
               <li className="tag-list-remained">{`+${
-                activeTags.length + activeCustomers.length - maxTagCount
+                validItems[tab].length - maxTagCount
               }개 더보기`}</li>
             )}
           </ul>
@@ -147,90 +153,52 @@ export default function TagList({
             style={{ width: `${triggerWidth}px` }}
           >
             <div className="tag-list-header">
-              <div className="tab-ui">
-                <button
-                  className={`tab-button ${tab === "tag" ? "active" : ""}`}
-                  onClick={() => setTab("tag")}
-                >
-                  태그
-                </button>
-                <button
-                  className={`tab-button ${tab === "customer" ? "active" : ""}`}
-                  onClick={() => setTab("customer")}
-                >
-                  고객
-                </button>
-              </div>
+              <TabBar tabs={TAG_TABS} activeTab={tab} setActiveTab={setTab} />
               <div className="tag-list-input-wrapper body-small">
                 <input
                   type="text"
                   ref={inputRef}
                   autoFocus
-                  placeholder={tab === "tag" ? "태그 추가" : "고객 추가"}
+                  placeholder={`${tab} 추가`}
                   onKeyDown={handleKeyDown}
                 />
               </div>
             </div>
             <ul className="tag-list tag-list-column">
-              {(tab === "tag" ? activeTags : activeCustomers).map(
-                (item, index) => (
-                  <li
-                    key={item.tagName}
-                    className={`tag-list-item tag-list-tag-${item.tagColor}`}
+              {validItems[tab].map((item, index) => (
+                <li
+                  key={item.tagName}
+                  className={`tag-list-item tag-list-tag-${item.tagColor}`}
+                >
+                  {editingItem &&
+                  editingItem.type === tab &&
+                  editingItem.index === index ? (
+                    <input
+                      type="text"
+                      value={editingItem.text}
+                      onChange={(e) =>
+                        setEditingItem({
+                          ...editingItem,
+                          text: e.target.value,
+                        })
+                      }
+                      onBlur={saveEditedItem}
+                      onKeyDown={saveEditedItemOnEnter}
+                      autoFocus
+                    />
+                  ) : (
+                    <span onClick={handleSetEditingItem(item.tagName, index)}>
+                      {`${TAG_PREFIX[tab as keyof typeof TAG_PREFIX]}${item.tagName}`}
+                    </span>
+                  )}
+                  <button
+                    className="tag-list-delete"
+                    onClick={handleDeleteTag(index)}
                   >
-                    {editingItem &&
-                    editingItem.type === tab &&
-                    editingItem.index === index ? (
-                      <input
-                        type="text"
-                        value={editingItem.text}
-                        onChange={(e) =>
-                          setEditingItem({
-                            ...editingItem,
-                            text: e.target.value,
-                          })
-                        }
-                        onBlur={() => saveEditedItem()}
-                        onKeyDown={(e) => {
-                          if (e.key === "Enter") {
-                            saveEditedItem();
-                          }
-                        }}
-                        autoFocus
-                      />
-                    ) : (
-                      <span
-                        onClick={() =>
-                          setEditingItem({
-                            type: tab,
-                            index,
-                            text: item.tagName,
-                          })
-                        }
-                      >
-                        {tab === "tag" ? "#" : "@"}
-                        {item.tagName}
-                      </span>
-                    )}
-                    <button
-                      className="tag-list-delete"
-                      onClick={() => {
-                        if (tab === "tag") {
-                          setActiveTags((prev) =>
-                            prev.filter((_, i) => i !== index),
-                          );
-                        } else {
-                          setActiveCustomers((prev) =>
-                            prev.filter((_, i) => i !== index),
-                          );
-                        }
-                      }}
-                    >
-                      <XIcon viewBox="0 0 20 20" />
-                    </button>
-                  </li>
-                ),
-              )}
+                    <XIcon viewBox="0 0 20 20" />
+                  </button>
+                </li>
+              ))}
             </ul>
           </Popover.Content>
         </Popover.Portal>
