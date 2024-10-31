8923da8ad1fadf4fb1c8639c1f8df9b93a7aafca
yulmam <yulmam1@naver.com>
2024-10-31 13:31:02 +0900

    feat : mypage layout 구조

--- src/app/(pages)/mypage/page.tsx
+++ src/app/(pages)/mypage/page.tsx
@@ -2,10 +2,13 @@
 
 import UserInfoCard from "@/app/components/common/card/UserInfoCard";
 import Header from "@/app/components/layout/Header";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./page.css";
+import TagList from "@/app/components/common/tag/TagList";
 
 export default function Mypage() {
+  //useState로 tags, customer 관리하기
+
   useEffect(() => {
     // 로그인 유무 확인해서 안되어 있으면 login 페이지로 보내기!
   }, []);
@@ -15,7 +18,11 @@ export default function Mypage() {
       <Header title="마이페이지" />
       <div className="mypage-user-container">
         <UserInfoCard name="김싸피" customerCount={0} sentMessageCount={0} />
-        <div className="mypage-content-container"></div>
+        <div className="mypage-content-container">
+          <p className="body-strong">태그 0</p>
+          {/* 태그 리스트 */}
+          <TagList tags={[{ tagName: "test", tagColor: "BLUE" }]} />
+        </div>
       </div>
     </div>
   );
