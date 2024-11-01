3517814cf41aa2998076b128625f9139ec314df3
yulmam <yulmam1@naver.com>
2024-11-01 14:02:06 +0900

    feat : 커스텀 페이지 고객 태그 표시

--- src/app/(pages)/customized/page.tsx
+++ src/app/(pages)/customized/page.tsx
@@ -25,57 +25,57 @@ interface CustomTemplate {
 type ApiResponse = CustomTemplate[];
 
 // dummyData 선언
-const dummyData: ApiResponse = [
-  {
-    templateId: 1,
-    templateTitle: "직장인 템플릿",
-    templateContent: "이 템플릿의 목적",
-    templateUsageCount: 10,
-    templateCreatedAt: "2024-10-25T01:22:27",
-    templateTags: [{ tagId: 1, tagName: "직장인", tagColor: "GREEN" }],
-    templateCustomers: [
-      { customerId: 12, customerName: "홍길동", customerColor: "GREEN" },
-    ],
-  },
-  {
-    templateId: 2,
-    templateTitle: "학생 템플릿",
-    templateContent: "이 템플릿의 목적",
-    templateUsageCount: 5,
-    templateCreatedAt: "2024-10-26T01:22:27",
-    templateTags: [{ tagId: 2, tagName: "학생", tagColor: "PINK" }],
-    templateCustomers: [
-      { customerId: 13, customerName: "김철수", customerColor: "PINK" },
-    ],
-  },
-  {
-    templateId: 3,
-    templateTitle: "자동차 템플릿",
-    templateContent: "이 템플릿의 목적",
-    templateUsageCount: 3,
-    templateCreatedAt: "2024-10-27T01:22:27",
-    templateTags: [{ tagId: 3, tagName: "자동차", tagColor: "SALMON" }],
-    templateCustomers: [
-      { customerId: 14, customerName: "이영희", customerColor: "SALMON" },
-    ],
-  },
-  {
-    templateId: 4,
-    templateTitle: "단골 고객 템플릿",
-    templateContent: "이 템플릿의 목적",
-    templateUsageCount: 8,
-    templateCreatedAt: "2024-10-28T01:22:27",
-    templateTags: [{ tagId: 4, tagName: "단골 고객", tagColor: "RED" }],
-    templateCustomers: [
-      { customerId: 15, customerName: "박수진", customerColor: "RED" },
-    ],
-  },
-];
+// const dummyData: ApiResponse = [
+//   {
+//     templateId: 1,
+//     templateTitle: "직장인 템플릿",
+//     templateContent: "이 템플릿의 목적",
+//     templateUsageCount: 10,
+//     templateCreatedAt: "2024-10-25T01:22:27",
+//     templateTags: [{ tagId: 1, tagName: "직장인", tagColor: "GREEN" }],
+//     templateCustomers: [
+//       { customerId: 12, customerName: "홍길동", customerColor: "GREEN" },
+//     ],
+//   },
+//   {
+//     templateId: 2,
+//     templateTitle: "학생 템플릿",
+//     templateContent: "이 템플릿의 목적",
+//     templateUsageCount: 5,
+//     templateCreatedAt: "2024-10-26T01:22:27",
+//     templateTags: [{ tagId: 2, tagName: "학생", tagColor: "PINK" }],
+//     templateCustomers: [
+//       { customerId: 13, customerName: "김철수", customerColor: "PINK" },
+//     ],
+//   },
+//   {
+//     templateId: 3,
+//     templateTitle: "자동차 템플릿",
+//     templateContent: "이 템플릿의 목적",
+//     templateUsageCount: 3,
+//     templateCreatedAt: "2024-10-27T01:22:27",
+//     templateTags: [{ tagId: 3, tagName: "자동차", tagColor: "SALMON" }],
+//     templateCustomers: [
+//       { customerId: 14, customerName: "이영희", customerColor: "SALMON" },
+//     ],
+//   },
+//   {
+//     templateId: 4,
+//     templateTitle: "단골 고객 템플릿",
+//     templateContent: "이 템플릿의 목적",
+//     templateUsageCount: 8,
+//     templateCreatedAt: "2024-10-28T01:22:27",
+//     templateTags: [{ tagId: 4, tagName: "단골 고객", tagColor: "RED" }],
+//     templateCustomers: [
+//       { customerId: 15, customerName: "박수진", customerColor: "RED" },
+//     ],
+//   },
+// ];
 
 export default function CustomizedPage() {
   const router = useRouter();
   const [filteredCustomMessageTemplates, setFilteredMessageTemplates] =
-    useState<ApiResponse | null>(dummyData);
+    useState<ApiResponse>([]);
 
   useEffect(() => {
     const fetchCustomTemplates = async () => {
@@ -83,8 +83,7 @@ export default function CustomizedPage() {
         const token = "ACCESS_TOKEN";
         const data = await getCustomTemplatesAPI({ token });
         console.log(data);
-        // setFilteredMessageTemplates(data.result[0]?.generalTemplates);
-        setFilteredMessageTemplates(data);
+        setFilteredMessageTemplates(data.result);
       } catch (error) {
         console.error("Error fetching message:", error);
       }
@@ -103,8 +102,8 @@ export default function CustomizedPage() {
 
       {filteredCustomMessageTemplates?.map((message) => (
         <Link
-          key={message?.templateId}
-          href={`/customized/${message?.templateId}`}
+          key={message.templateId}
+          href={`/customized/${message.templateId}`}
         >
           <CustomizedCard
             title={message?.templateTitle}
