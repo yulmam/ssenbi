b3838495a042665bdfc2c4688a821b0c28e385a2
yulmam <yulmam1@naver.com>
2024-11-04 17:01:07 +0900

    feat: 로그인 버튼 추가

--- src/app/components/layout/Header.tsx
+++ src/app/components/layout/Header.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./Header.css";
 import BackIcon from "@/app/assets/svg/ChevronLeft.svg";
 import { useRouter } from "next/navigation";
+import useAuthStore from "@/stores/authStore";
 
 interface HeaderProps {
   title: string;
@@ -10,19 +11,54 @@ interface HeaderProps {
 
 export default function Header({ title, showBackIcon = false }: HeaderProps) {
   const router = useRouter();
+  const isLoggedIn = useAuthStore((state) => state.isLoggedIn);
+  const Logout = useAuthStore((state) => state.setLogout);
+
+  const handleLogin = () => {
+    router.push("/auth/login");
+  };
+
+  const handleLogout = () => {
+    Logout();
+    router.push("/");
+  };
+
+  useEffect(() => {
+    console.log(isLoggedIn);
+  }, [isLoggedIn]);
 
   return (
     <div className="header heading">
-      {showBackIcon && (
+      <div className="header_title-container">
+        {showBackIcon && (
+          <button
+            onClick={() => router.back()}
+            className="header_button-back"
+            aria-label="뒤로 가기"
+          >
+            <BackIcon />
+          </button>
+        )}
+        {title}
+      </div>
+      {isLoggedIn === true && (
         <button
-          onClick={() => router.back()}
-          className="header_button-back"
-          aria-label="뒤로 가기"
+          type="button"
+          className="header_button-auth"
+          onClick={handleLogout}
         >
-          <BackIcon />
+          로그아웃
         </button>
       )}
-      {title}
+      {isLoggedIn === false && (
+        <button
+          type="button"
+          className="header_button-auth"
+          onClick={handleLogin}
+        >
+          로그인
+        </button>
+      )}
     </div>
   );
 }
