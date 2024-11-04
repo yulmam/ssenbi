9e9b2d3ceaba310efa13efb9a1981bfdd8166c82
yulmam <yulmam1@naver.com>
2024-11-04 14:03:08 +0900

    hotfix: token validate

--- backend/Bobi/src/main/java/com/haneolenae/bobi/domain/auth/util/JwtTokenProvider.java
+++ backend/Bobi/src/main/java/com/haneolenae/bobi/domain/auth/util/JwtTokenProvider.java
@@ -17,8 +17,8 @@ import io.jsonwebtoken.security.Keys;
 public class JwtTokenProvider {
 	private Key secretKey = generateSecretKey(
 		"c14aedf77d1d17e7f3259f26a01c6fd9bd70b32b334a51509abc616386a3b67aa481573a9dda3bae5043cd44eecaeb79842cea930621baf23f198cceae9d8234");
-	private long accessTokenValidTime = 1 * 60 * 1000L;//1분 (차후 축소 예정)
-	private long refreshTokenValidTime = 720 * 60 * 1000L;//12시간 (분/초/밀리초)
+	private long accessTokenValidTime = 30 * 1440 * 60 * 1000L;//한달 (차후 축소 예정)
+	private long refreshTokenValidTime = 30 * 1440 * 60 * 1000L;//한달 (분/초/밀리초)
 
 	private Key generateSecretKey(String secret) {
 		byte[] keyBytes = Decoders.BASE64.decode(secret);
