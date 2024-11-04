45e1c67b973fcd6d7290536b218b5f1926fa500d
yulmam <yulmam1@naver.com>
2024-11-04 16:12:24 +0900

    feat : 커스텀 페이지 비어있을 때 UI 추가

--- src/app/(pages)/customized/page.tsx
+++ src/app/(pages)/customized/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import "./page.css";
 import Header from "@/app/components/layout/Header";
 import Link from "next/link";
 import FloatingActionButton from "@/app/components/common/button/FloatingActionButton";
@@ -10,6 +9,8 @@ import { useRouter } from "next/navigation";
 import { CustomerType, TagType } from "@/types/tag/tagTypes";
 import { getCustomTemplatesAPI } from "@/app/api/customized/customizedAPI";
 import Cookies from "js-cookie";
+import Image from "next/image";
+import "./page.css";
 
 // Custom Template 타입 정의
 interface CustomTemplate {
@@ -24,54 +25,6 @@ interface CustomTemplate {
 
 // ApiResponse 타입 정의
 type ApiResponse = CustomTemplate[];
-
-// dummyData 선언
-// const dummyData: ApiResponse = [
-//   {
-//     templateId: 1,
-//     templateTitle: "직장인 템플릿",
-//     templateContent: "이 템플릿의 목적",
-//     templateUsageCount: 10,
-//     templateCreatedAt: "2024-10-25T01:22:27",
-//     templateTags: [{ tagId: 1, tagName: "직장인", tagColor: "GREEN" }],
-//     templateCustomers: [
-//       { customerId: 12, customerName: "홍길동", customerColor: "GREEN" },
-//     ],
-//   },
-//   {
-//     templateId: 2,
-//     templateTitle: "학생 템플릿",
-//     templateContent: "이 템플릿의 목적",
-//     templateUsageCount: 5,
-//     templateCreatedAt: "2024-10-26T01:22:27",
-//     templateTags: [{ tagId: 2, tagName: "학생", tagColor: "PINK" }],
-//     templateCustomers: [
-//       { customerId: 13, customerName: "김철수", customerColor: "PINK" },
-//     ],
-//   },
-//   {
-//     templateId: 3,
-//     templateTitle: "자동차 템플릿",
-//     templateContent: "이 템플릿의 목적",
-//     templateUsageCount: 3,
-//     templateCreatedAt: "2024-10-27T01:22:27",
-//     templateTags: [{ tagId: 3, tagName: "자동차", tagColor: "SALMON" }],
-//     templateCustomers: [
-//       { customerId: 14, customerName: "이영희", customerColor: "SALMON" },
-//     ],
-//   },
-//   {
-//     templateId: 4,
-//     templateTitle: "단골 고객 템플릿",
-//     templateContent: "이 템플릿의 목적",
-//     templateUsageCount: 8,
-//     templateCreatedAt: "2024-10-28T01:22:27",
-//     templateTags: [{ tagId: 4, tagName: "단골 고객", tagColor: "RED" }],
-//     templateCustomers: [
-//       { customerId: 15, customerName: "박수진", customerColor: "RED" },
-//     ],
-//   },
-// ];
 
 export default function CustomizedPage() {
   const router = useRouter();
@@ -103,19 +56,33 @@ export default function CustomizedPage() {
     <div className="page-container">
       <Header title="커스텀" />
 
-      {filteredCustomMessageTemplates?.map((message) => (
-        <Link
-          key={message.templateId}
-          href={`/customized/${message.templateId}`}
-        >
-          <CustomizedCard
-            title={message?.templateTitle}
-            content={message?.templateContent}
-            tags={message?.templateTags}
-            customers={message?.templateCustomers}
-          />
-        </Link>
-      ))}
+      {filteredCustomMessageTemplates &&
+      filteredCustomMessageTemplates.length > 0 ? (
+        filteredCustomMessageTemplates.map((message) => (
+          <Link
+            key={message.templateId}
+            href={`/customized/${message.templateId}`}
+          >
+            <CustomizedCard
+              title={message?.templateTitle}
+              content={message?.templateContent}
+              tags={message?.templateTags}
+              customers={message?.templateCustomers}
+            />
+          </Link>
+        ))
+      ) : (
+        <div className="flex-container">
+          <div className="empty-message">
+            <p className="body-medium">새로운 메세지를 추가해주세요</p>
+            <Image
+              src="/assets/images/messageIcon.png"
+              fill
+              alt="메세지 icon"
+            />
+          </div>
+        </div>
+      )}
 
       <FloatingActionButton onClick={handleNewTemplate} text={"템플릿 추가"} />
     </div>
