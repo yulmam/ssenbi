f802353dd80c26494456e9262d76e63c353fe0c9
yulmam <yulmam1@naver.com>
2024-11-04 10:51:53 +0900

    hotfix: cors fix

--- backend/Bobi/src/main/java/com/haneolenae/bobi/global/config/WebConfig.java
+++ backend/Bobi/src/main/java/com/haneolenae/bobi/global/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
 			.allowedOriginPatterns("*") // 허용할 출처 : 특정 도메인만 받을 수 있음
-			.allowedMethods("GET", "POST", "DELETE", "PUT", "PATCH") // 허용할 HTTP method
+			.allowedMethods("GET", "POST", "DELETE", "PUT", "PATCH", "OPTIONS") // 허용할 HTTP method
 			.exposedHeaders("*")
 			.allowedHeaders("*")
 			.allowCredentials(true); // 쿠키 인증 요청 허용
