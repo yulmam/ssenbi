da8be469e74b4390762d21c9442dbd8263e60080
yulmam <yulmam1@naver.com>
2024-11-05 09:10:35 +0900

    hotfix: webConfig

--- backend/Bobi/src/main/java/com/haneolenae/bobi/global/config/WebConfig.java
+++ backend/Bobi/src/main/java/com/haneolenae/bobi/global/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
 		registry.addMapping("/**")
 			.allowedOrigins("https://www.ssenbi.co.kr", "http://localhost:3000", "http://localhost:3001",
 				"http://localhost:8080", "http://k11a109.p.ssafy.io:8080", "http://www.ssenbi.co.kr",
-				"ssenbi.co.kr") // 허용할 출처 : 특정 도메인만 받을 수 있음
+				"https://ssenbi.co.kr", "http://ssenbi.co.kr") // 허용할 출처 : 특정 도메인만 받을 수 있음
 			.allowedMethods("GET", "POST", "DELETE", "PUT", "PATCH", "OPTIONS") // 허용할 HTTP method
 			.allowedHeaders("*")
 			.allowCredentials(true); // 쿠키 인증 요청 허용
