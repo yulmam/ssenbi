1920f7d3215ba6d9046b77b022db196bb2725a06
yulmam <yulmam1@naver.com>
2024-11-07 14:31:09 +0900

    feat : mypage API 연결

--- src/app/(pages)/auth/mypage/page.tsx
+++ src/app/(pages)/auth/mypage/page.tsx
@@ -6,7 +6,14 @@ import { useEffect, useState } from "react";
 import "./page.css";
 import TagList from "@/app/components/common/tag/TagList";
 import { TagType } from "@/types/tag/tagTypes";
+import { getMemberOverViewAPI } from "@/app/api/member/memberAPI";
+import HashLoading from "@/app/components/common/loading/HashLoading";
 
+interface MemberOverViewType {
+  name: string;
+  customerCount: number;
+  messageCount: number;
+}
 const DUMMY_DATA: TagType[] = [
   {
     tagName: "test",
@@ -15,18 +22,45 @@ const DUMMY_DATA: TagType[] = [
   },
 ];
 
+const INITIALMEMBERDATA = {
+  name: "",
+  customerCount: 0,
+  messageCount: 0,
+};
 export default function MypagePage() {
+  const [isLoading, setIsLoading] = useState<boolean>(true);
   const [tags, setTags] = useState<TagType[]>(DUMMY_DATA);
+  const [memberData, setMemberData] =
+    useState<MemberOverViewType>(INITIALMEMBERDATA);
 
   useEffect(() => {
-    // 로그인 유무 확인해서 안되어 있으면 login 페이지로 보내기!
+    fetchMemberData();
   }, []);
 
+  const fetchMemberData = async () => {
+    try {
+      const response = await getMemberOverViewAPI();
+      console.log(response);
+      setMemberData(response.result);
+    } catch (err) {
+      console.error(err);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  if (isLoading) {
+    return <HashLoading />;
+  }
   return (
     <div className="page-container">
       <Header title="마이페이지" />
       <div className="mypage-user-container">
-        <UserInfoCard name="김싸피" customerCount={0} sentMessageCount={0} />
+        <UserInfoCard
+          name={memberData.name}
+          customerCount={memberData.customerCount}
+          sentMessageCount={memberData.messageCount}
+        />
         <div className="mypage-content-container">
           <p className="body-strong">태그 0</p>
           {/* 태그 리스트 */}
