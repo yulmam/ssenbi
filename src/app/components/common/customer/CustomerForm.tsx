ae03e8acfd924833f9e95c6e5fbf4d5d8a552075
yulmam <yulmam1@naver.com>
2024-11-08 13:35:05 +0900

    feat: 고객 추가/수정/삭제 기능 구현

---
+++ src/app/components/common/customer/CustomerForm.tsx
@@ -0,0 +1,143 @@
+"use client";
+
+import {
+  postCustomerAPI,
+  putCustomerAPI,
+} from "@/app/api/customer/customerAPI";
+import TagList from "@/app/components/common/tag/TagList";
+import {
+  CustomerCreationType,
+  CustomerType,
+} from "@/types/customer/customerType";
+import { TagType } from "@/types/tag/tagTypes";
+import getRandomTagColor from "@/utils/getRandomTagColor";
+import { useRouter } from "next/navigation";
+import { useEffect, useRef, useState } from "react";
+import "./CustomerForm.css";
+
+interface CustomerFormProps {
+  customer?: CustomerType;
+}
+
+export default function CustomerForm({ customer }: CustomerFormProps) {
+  const router = useRouter();
+  const nameRef = useRef<HTMLInputElement>(null);
+  const ageRef = useRef<HTMLInputElement>(null);
+  const phoneNumberRef = useRef<HTMLInputElement>(null);
+  const memoRef = useRef<HTMLTextAreaElement>(null);
+  const [isMale, setIsMale] = useState<boolean>(true);
+  const [tags, setTags] = useState<TagType[]>([]);
+
+  useEffect(() => {
+    if (!customer) {
+      return;
+    }
+    if (nameRef.current) {
+      nameRef.current.value = customer.customerName;
+    }
+    if (ageRef.current) {
+      ageRef.current.value = customer.customerAge.toString();
+    }
+    if (phoneNumberRef.current) {
+      phoneNumberRef.current.value = customer.customerPhoneNumber;
+    }
+    if (memoRef.current) {
+      memoRef.current.value = customer.customerMemo;
+    }
+    setIsMale(customer.customerGender === "MALE");
+    setTags(customer.customerTags);
+  }, [customer]);
+
+  const handleCreateCustomer = async () => {
+    const customerName = nameRef.current?.value?.trim() || "";
+    const customerAge = Number(ageRef.current?.value) || 0;
+    const customerGender = isMale ? "MALE" : "FEMALE";
+    const customerPhoneNumber = phoneNumberRef.current?.value?.trim() || "";
+    const customerTags = tags.map((tag) => tag.tagId);
+    const customerMemo = memoRef.current?.value?.trim() || "";
+    const customerColor = getRandomTagColor();
+    const customerData: CustomerCreationType = {
+      customerName,
+      customerAge,
+      customerGender,
+      customerPhoneNumber,
+      customerTags,
+      customerMemo,
+      customerColor,
+    };
+
+    try {
+      const { result } = customer
+        ? await putCustomerAPI(customer.customerId, customerData)
+        : await postCustomerAPI(customerData);
+      router.replace(
+        result.customerId ? `/customer/${result.customerId}` : "customer",
+      );
+    } catch {
+      alert("고객 추가에 실패했습니다.");
+    }
+  };
+
+  return (
+    <>
+      <div className="field-wrapper ">
+        <span className="body label">이름</span>
+        <input
+          className="form-input-field"
+          ref={nameRef}
+          type="text"
+          maxLength={20}
+        />
+      </div>
+      <div className="field-wrapper">
+        <span className="body label">나이</span>
+        <input className="form-input-field" ref={ageRef} type="number" />
+      </div>
+      <div className="field-wrapper">
+        <span className="body label">성별</span>
+        <div className="toggle-wrapper">
+          <button
+            className={`gender-selector body-medium ${isMale ? "selected" : ""}`}
+            onClick={() => setIsMale(true)}
+          >
+            남자
+          </button>
+          <button
+            className={`gender-selector body-medium ${!isMale ? "selected" : ""}`}
+            onClick={() => setIsMale(false)}
+          >
+            여자
+          </button>
+        </div>
+      </div>
+      <div className="field-wrapper">
+        <span className="body label">전화번호</span>
+        <input
+          className="form-input-field"
+          ref={phoneNumberRef}
+          type="number"
+          maxLength={13}
+        />
+      </div>
+      <div className="field-wrapper">
+        <label className="label">태그</label>
+        <TagList tags={tags} setTags={setTags} />
+      </div>
+      <div className="field-wrapper">
+        <label className="label">메모</label>
+        <textarea ref={memoRef} className="body memo form-input-field" />
+      </div>
+      <div className="button-group">
+        <button onClick={router.back} className="button body-strong">
+          취소
+        </button>
+        <button
+          onClick={handleCreateCustomer}
+          className="button primary body-strong"
+        >
+          {`${customer ? "수정" : "추가"}`}
+        </button>
+      </div>
+    </>
+  );
+}
