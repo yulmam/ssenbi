7683a179e1780e53f8b16f8d66b5fb04506d4200
yulmam <yulmam1@naver.com>
2024-11-14 13:05:07 +0900

    feat : 필터 레이어

--- src/app/components/common/customized/CustomizedList.tsx
+++ src/app/components/common/customized/CustomizedList.tsx
@@ -149,6 +149,7 @@ export default function CustomizedList() {
             <CustomerTagList
               customers={selectedCustomers}
               setCustomers={setSelectedCustomers}
+              isFilter={true}
             />
           </div>
         </div>
